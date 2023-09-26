b12faf970e599d6c9b1506b49f96d46de980d8c9
Akayy <techakayy@gmail.com>
2023-09-26 16:53:29 +1000

    Alignment

--- src/app.ts
+++ src/app.ts
@@ -8,19 +8,15 @@
 //   window.devtools = devtools
 // }
 
-import { defineApp } from 'iles'
 import 'uno.css'
 import '@/assets/css/tailwind.css'
 import 'prismjs/themes/prism-tomorrow.css'
 
-import { headConfig } from '@/plugins/head'
+import { defineApp } from 'iles'
 import pinia from '@/plugins/pinia'
 
 export default defineApp({
   enhanceApp({ app }) {
     app.use(pinia)
-  },
-  head({ frontmatter, site }) {
-    return headConfig({ frontmatter, site })
   },
 })
--- src/components/MetaTags.vue
+++ src/components/MetaTags.vue
@@ -1,17 +1,97 @@
 <script setup lang="ts">
-  import site from '@/site'
+  import { useSeoMeta } from 'unhead'
+
+  import checkDarkTheme from '@/composables/dark-color-scheme-check?raw'
+  import type { Script } from '@unhead/schema'
+  type TurboScript = Script & { once: true }
+
+  import siteDefn from '@/site'
   import image from '@/screenshots/image.jpg'
-  const { title, description, url } = site
+  import { pg_font_urls } from '~~/themes/pg-tailwindcss/tokens.mjs'
+
+  const link: any = [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.svg',
+    },
+  ]
+  const noscript: any = []
+
+  if (pg_font_urls.length) {
+    const googleapis = 'https://fonts.googleapis.com'
+    const gstatic = 'https://fonts.gstatic.com'
+    link.push(
+      { rel: 'dns-prefetch', href: googleapis },
+      { rel: 'dns-prefetch', href: gstatic },
+      { rel: 'preconnect', crossorigin: 'anonymous', href: googleapis },
+      { rel: 'preconnect', crossorigin: 'anonymous', href: gstatic },
+      {
+        rel: 'preload',
+        as: 'style',
+        onload: "this.onload=null;this.rel='stylesheet'",
+        href: pg_font_urls.toString(),
+      },
+    )
+    noscript.push(
+      `<link rel="stylesheet" crossorigin="anonymous" href="${pg_font_urls.toString()}" />`,
+    )
+  }
+
+  const { title, description, url, author } = siteDefn
+
+  const { frontmatter, site } = usePage()
+  const imgUrl = $computed(() => `${site.url}${image}`)
+
+  useSeoMeta({
+    // charset: 'utf-8',
+    // author,
+    // viewport: 'width=device-width, initial-scale=1',
+    // keywords: route.meta.tags?.toString(),
+    // title,
+    description,
+    ogTitle: title,
+    ogDescription: description,
+    ogImage: imgUrl,
+    ogImageAlt: title,
+    // og:image:width
+    // og:image:height
+    // og:image:alt
+    // og:image: type
+    // og:image: secure_url
+    ogUrl: url,
+    ogSiteName: title,
+    // og: locale
+    // og: type
+    twitterTitle: title,
+    twitterDescription: description,
+    twitterImage: imgUrl,
+    twitterImageAlt: title,
+    twitterSite: url,
+    twitterCard: 'summary_large_image',
+  })
+
+  useHead({
+    // title,
+    // titleTemplate: (titleChunk) => {
+    //   return titleChunk ? `${titleChunk} - ${title}` : title
+    // },
+    htmlAttrs: { lang: 'en-US' },
+    meta: [
+      { name: 'charset', content: 'utf-8' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      { name: 'author', content: author },
+      { name: 'keywords', content: computed(() => frontmatter.tags) },
+    ],
+    script: [{ innerHTML: checkDarkTheme, once: true } as TurboScript],
+    link,
+    noscript,
+  })
 </script>
 
 <template>
-  <Head>
-    <meta property="twitter:title" :content="title" />
-    <meta property="twitter:description" :content="description" />
-    <meta property="twitter:image" :content="image" />
-    <meta property="twitter:image:alt" :content="title" />
-    <meta property="twitter:site" :content="url" />
-    <meta property="twitter:card" content="summary_large_image" />
-    <!-- <meta property="twitter:card" content="summary" /> -->
-  </Head>
+  <div></div>
 </template>
