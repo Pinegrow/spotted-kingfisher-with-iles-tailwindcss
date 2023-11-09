657d26316f0b3102763ca2cfd78fe3eddb71891d
Akayy <techakayy@gmail.com>
2023-11-10 09:15:51 +1100

    Updated unocss

--- src/site.ts
+++ src/site.ts
@@ -1,5 +1,9 @@
+// since `.js, .ts` files are not included by default,
+// the following comment tells UnoCSS to force scan this file (to pick the logo icon).
+// @unocss-include
+
 export default {
-  title: `Spotted Kingfisher`,
+  title: 'Spotted Kingfisher',
   description: `Help the world's most iconic species`,
   author: 'Pinegrow',
   url: 'https://spotted-kingfisher-iles-tailwindcss.netlify.app',
